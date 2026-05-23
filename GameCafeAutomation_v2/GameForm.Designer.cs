2a1c9a50444f40df99145320a5e229d6b58f1c6c
Yasin Ubuz <yasinubuz4@gmail.com>
2026-05-23 14:19:20 +0300

    Oyun formu tamamlandı

--- GameCafeAutomation_v2/GameForm.Designer.cs
+++ GameCafeAutomation_v2/GameForm.Designer.cs
@@ -28,19 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_isim = new System.Windows.Forms.Label();
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.lbl_kategori = new System.Windows.Forms.Label();
+            this.txt_kategori = new System.Windows.Forms.TextBox();
+            this.lbl_ucret = new System.Windows.Forms.Label();
+            this.txt_ucret = new System.Windows.Forms.TextBox();
+            this.btn_listele = new System.Windows.Forms.Button();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_guncelle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lbl_isim
+            // 
+            this.lbl_isim.AutoSize = true;
+            this.lbl_isim.Location = new System.Drawing.Point(60, 54);
+            this.lbl_isim.Name = "lbl_isim";
+            this.lbl_isim.Size = new System.Drawing.Size(28, 13);
+            this.lbl_isim.TabIndex = 0;
+            this.lbl_isim.Text = " İsim";
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Location = new System.Drawing.Point(119, 54);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(100, 20);
+            this.txt_isim.TabIndex = 1;
+            // 
+            // lbl_kategori
+            // 
+            this.lbl_kategori.AutoSize = true;
+            this.lbl_kategori.Location = new System.Drawing.Point(42, 104);
+            this.lbl_kategori.Name = "lbl_kategori";
+            this.lbl_kategori.Size = new System.Drawing.Size(46, 13);
+            this.lbl_kategori.TabIndex = 2;
+            this.lbl_kategori.Text = "Kategori";
+            // 
+            // txt_kategori
+            // 
+            this.txt_kategori.Location = new System.Drawing.Point(119, 101);
+            this.txt_kategori.Name = "txt_kategori";
+            this.txt_kategori.Size = new System.Drawing.Size(100, 20);
+            this.txt_kategori.TabIndex = 3;
+            // 
+            // lbl_ucret
+            // 
+            this.lbl_ucret.AutoSize = true;
+            this.lbl_ucret.Location = new System.Drawing.Point(20, 148);
+            this.lbl_ucret.Name = "lbl_ucret";
+            this.lbl_ucret.Size = new System.Drawing.Size(68, 13);
+            this.lbl_ucret.TabIndex = 4;
+            this.lbl_ucret.Text = "Saatlik Ücret";
+            // 
+            // txt_ucret
+            // 
+            this.txt_ucret.Location = new System.Drawing.Point(119, 141);
+            this.txt_ucret.Name = "txt_ucret";
+            this.txt_ucret.Size = new System.Drawing.Size(100, 20);
+            this.txt_ucret.TabIndex = 5;
+            // 
+            // btn_listele
+            // 
+            this.btn_listele.Location = new System.Drawing.Point(23, 308);
+            this.btn_listele.Name = "btn_listele";
+            this.btn_listele.Size = new System.Drawing.Size(164, 49);
+            this.btn_listele.TabIndex = 6;
+            this.btn_listele.Text = "Listele";
+            this.btn_listele.UseVisualStyleBackColor = true;
+            this.btn_listele.Click += new System.EventHandler(this.btn_listele_Click);
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(218, 308);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(164, 49);
+            this.btn_ekle.TabIndex = 7;
+            this.btn_ekle.Text = "Ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // btn_guncelle
+            // 
+            this.btn_guncelle.Location = new System.Drawing.Point(413, 308);
+            this.btn_guncelle.Name = "btn_guncelle";
+            this.btn_guncelle.Size = new System.Drawing.Size(164, 49);
+            this.btn_guncelle.TabIndex = 8;
+            this.btn_guncelle.Text = "Güncelle";
+            this.btn_guncelle.UseVisualStyleBackColor = true;
+            this.btn_guncelle.Click += new System.EventHandler(this.btn_guncelle_Click);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Location = new System.Drawing.Point(608, 308);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(164, 49);
+            this.btn_sil.TabIndex = 9;
+            this.btn_sil.Text = "Sil";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(395, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(377, 195);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_guncelle);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.btn_listele);
+            this.Controls.Add(this.txt_ucret);
+            this.Controls.Add(this.lbl_ucret);
+            this.Controls.Add(this.txt_kategori);
+            this.Controls.Add(this.lbl_kategori);
+            this.Controls.Add(this.txt_isim);
+            this.Controls.Add(this.lbl_isim);
             this.Name = "GameForm";
             this.Text = "Oyun Yönetimi";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_isim;
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.Label lbl_kategori;
+        private System.Windows.Forms.TextBox txt_kategori;
+        private System.Windows.Forms.Label lbl_ucret;
+        private System.Windows.Forms.TextBox txt_ucret;
+        private System.Windows.Forms.Button btn_listele;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_guncelle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- GameCafeAutomation_v2/GameForm.cs
+++ GameCafeAutomation_v2/GameForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GameCafeAutomation.Data;
+using GameCafeAutomation.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,92 @@ namespace GameCafeAutomation_v2
 {
     public partial class GameForm : Form
     {
+        GameCafeDbContext db = new GameCafeDbContext();
+
         public GameForm()
         {
             InitializeComponent();
+            btn_listele.PerformClick();
+
+        }
+
+        private void btn_listele_Click(object sender, EventArgs e)
+        {
+            try { dataGridView1.DataSource = db.Games.ToList(); }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Listeleme: " + ex.Message);
+            }
+        }
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Game g = new Game();
+                g.Game_name = txt_isim.Text;
+                g.Game_category = txt_kategori.Text;
+                g.Hourly_price = txt_ucret.Text;
+                db.Games.Add(g);
+                db.SaveChanges();
+                MessageBox.Show("Kayıt Eklendi!");
+                btn_listele.PerformClick();
+            }
+            catch (Exception ex) { MessageBox.Show("Ekleme: " + ex.Message); }
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow == null) return;
+                int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["Game_id"].Value);
+                Game g = db.Games.Find(id);
+                if (g != null)
+                {
+                    db.Games.Remove(g);
+                    db.SaveChanges();
+                    MessageBox.Show("Kayıt Silindi!");
+                    btn_listele.PerformClick();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Silme: " + ex.Message); }
+        }
+
+        private void btn_guncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow != null)
+                {
+                    int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["Game_id"].Value);
+                    Game g = db.Games.Find(id);
+                    if (g != null)
+                    {
+                        g.Game_name = txt_isim.Text;
+                        g.Game_category = txt_kategori.Text;
+                        g.Hourly_price = txt_ucret.Text;
+                        db.SaveChanges();
+                        MessageBox.Show("Kayıt Güncellendi!");
+                        btn_listele.PerformClick();
+                    }
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Güncelleme: " + ex.Message); }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                txt_isim.Text = dataGridView1.Rows[e.RowIndex].Cells["Game_name"].Value.ToString();
+                txt_kategori.Text = dataGridView1.Rows[e.RowIndex].Cells["Game_category"].Value.ToString();
+                txt_ucret.Text = dataGridView1.Rows[e.RowIndex].Cells["Hourly_price"].Value.ToString();
+            }
         }
     }
+    
 }
+   
+
+
